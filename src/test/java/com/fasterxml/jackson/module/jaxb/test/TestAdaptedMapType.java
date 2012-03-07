bb0717ee70094f8ee4c572bab02710de11ae83ae
Tatu Saloranta <tsaloranta@gmail.com>
2012-03-06 20:28:37 -0800

    ...

--- src/test/java/com/fasterxml/jackson/module/jaxb/test/TestAdaptedMapType.java
+++ src/test/java/com/fasterxml/jackson/module/jaxb/test/TestAdaptedMapType.java
@@ -1,32 +1,29 @@
 package com.fasterxml.jackson.module.jaxb.test;
 
+import java.io.*;
+import java.util.HashMap;
+
 import junit.framework.TestCase;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.HashMap;
-
 /**
  * @author Ryan Heaton
  */
 public class TestAdaptedMapType extends TestCase {
 
-  public void testJacksonAdaptedMapType() throws Exception {
+  public void testJacksonAdaptedMapType() throws IOException {
     ObjectContainingAMap obj = new ObjectContainingAMap();
     obj.setMyMap(new HashMap<String, String>());
     obj.getMyMap().put("this", "that");
     obj.getMyMap().put("how", "here");
 
     ObjectMapper mapper = new ObjectMapper();
-    mapper.getDeserializationConfig().withAnnotationIntrospector(new JaxbAnnotationIntrospector());
-    mapper.getSerializationConfig().withAnnotationIntrospector(new JaxbAnnotationIntrospector());
-    ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-    mapper.writeValue(bytesOut, obj);
-    obj = mapper.readValue(new ByteArrayInputStream(bytesOut.toByteArray()), ObjectContainingAMap.class);
+    mapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
+    byte[] json = mapper.writeValueAsBytes(obj);
+    obj = mapper.readValue(new ByteArrayInputStream(json), ObjectContainingAMap.class);
     assertNotNull(obj.getMyMap());
   }
 }
