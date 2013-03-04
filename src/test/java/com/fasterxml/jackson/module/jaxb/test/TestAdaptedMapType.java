b3867ee7e1c1feacd261f9a78321c289639baf65
Tatu Saloranta <tsaloranta@gmail.com>
2013-03-03 18:17:17 -0800

    Bit of cleanup

--- src/test/java/com/fasterxml/jackson/module/jaxb/test/TestAdaptedMapType.java
+++ src/test/java/com/fasterxml/jackson/module/jaxb/test/TestAdaptedMapType.java
@@ -2,6 +2,9 @@ package com.fasterxml.jackson.module.jaxb.test;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -12,16 +15,29 @@ import com.fasterxml.jackson.module.jaxb.BaseJaxbTest;
  */
 public class TestAdaptedMapType extends BaseJaxbTest
 {
+    static class ObjectContainingAMap {
 
-  public void testJacksonAdaptedMapType() throws IOException {
-    ObjectContainingAMap obj = new ObjectContainingAMap();
-    obj.setMyMap(new HashMap<String, String>());
-    obj.getMyMap().put("this", "that");
-    obj.getMyMap().put("how", "here");
+        private Map<String, String> myMap;
 
-    ObjectMapper mapper = getJaxbMapper();
-    byte[] json = mapper.writeValueAsBytes(obj);
-    obj = mapper.readValue(new ByteArrayInputStream(json), ObjectContainingAMap.class);
-    assertNotNull(obj.getMyMap());
-  }
+        @XmlJavaTypeAdapter( MapAdapter.class )
+        public Map<String, String> getMyMap() {
+          return myMap;
+        }
+
+        public void setMyMap(Map<String, String> myMap) {
+          this.myMap = myMap;
+        }
+    }
+    
+    public void testJacksonAdaptedMapType() throws IOException {
+        ObjectContainingAMap obj = new ObjectContainingAMap();
+        obj.setMyMap(new HashMap<String, String>());
+        obj.getMyMap().put("this", "that");
+        obj.getMyMap().put("how", "here");
+
+        ObjectMapper mapper = getJaxbMapper();
+        byte[] json = mapper.writeValueAsBytes(obj);
+        obj = mapper.readValue(new ByteArrayInputStream(json), ObjectContainingAMap.class);
+        assertNotNull(obj.getMyMap());
+    }
 }
